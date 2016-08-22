0d9720d76a55dc56bab8722c4a425c46eb4b06ed
Aidan Feldman <aidan.feldman@gsa.gov>
2016-08-22 01:50:22 -0400

    test for files to exist

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-exit 0
+set -ex
+
+# YOUR CODE HERE
--- test_static.py
+++ test_static.py
@@ -1,8 +1,14 @@
-import tempfile
+import os
 import subprocess
+import tempfile
 
 def test_simple():
     with tempfile.TemporaryDirectory(suffix="static") as tmpdirname:
         result = subprocess.run(["./generate.sh", "examples/simple/", tmpdirname])
         assert result.returncode == 0
-        # TODO check that appropriate files exist with appropriate content
+
+        files = os.listdir(tmpdirname)
+        files.sort()
+        assert files == ["post1.html", "post2.html"]
+
+        # TODO check that files have appropriate content
