990ff0b81080e90f99e84b7ebfac6f52d552e500
Aidan Feldman <aidan.feldman@gsa.gov>
2016-09-14 11:14:48 -0400

    test contents of files

--- test_static.py
+++ test_static.py
@@ -1,14 +1,29 @@
 import os
 import subprocess
 import tempfile
+from bs4 import BeautifulSoup
 
 def test_simple():
-    with tempfile.TemporaryDirectory(suffix="static") as tmpdirname:
-        result = subprocess.run(["./generate.sh", "examples/simple/", tmpdirname])
+    with tempfile.TemporaryDirectory(suffix='static') as tmpdirname:
+        result = subprocess.run(['./generate.sh', 'examples/simple/', tmpdirname])
         assert result.returncode == 0
 
         files = os.listdir(tmpdirname)
         files.sort()
-        assert files == ["post1.html", "post2.html"]
+        assert files == ['post1.html', 'post2.html']
 
-        # TODO check that files have appropriate content
+        # check that files have appropriate content
+
+        path = os.path.join(tmpdirname, files[0])
+        soup = BeautifulSoup(open(path), 'html.parser')
+        title = soup.find('title')
+        assert title.string.strip() == "Post 1 Title"
+        body = soup.find('body')
+        assert body.string.strip() == "This is the body of Post 1."
+
+        path = os.path.join(tmpdirname, files[1])
+        soup = BeautifulSoup(open(path), 'html.parser')
+        title = soup.find('title')
+        assert title.string.strip() == "Post 2 Title"
+        body = soup.find('body')
+        assert body.string.strip() == "This is the body of Post 2."
