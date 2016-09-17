dc12ed1bc5bb38c4f53226452ca099843cce691f
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-09-17 16:48:47 +0000

    completed assignment

--- generate.sh
+++ generate.sh
@@ -2,4 +2,26 @@
 
 set -ex
 
-# YOUR CODE HERE
+#exemplar directory does exist
+if [ -d "$1" ]; then
+  # output does not exist, make it
+  if [ ! -d "$2" ]; then
+    mkdir $2
+  fi
+
+  #identify all files
+  for file in $1/*.txt;
+  do
+    title="$(cat $file | head -n 1)"
+    body="$(cat $file | tail -n 1)"
+    base="$(basename $file .txt)"
+    sed "s/{{title}}/$title/g;s/{{body}}/$body/g" template.html >> $2/$base.html
+
+  done
+
+fi
+#sed 's/{{title}}/Welcome to the Island/g' template.html
+#references
+# http://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
+# http://stackoverflow.com/questions/26568952/how-to-replace-multiple-patterns-at-once-with-sed
+#
