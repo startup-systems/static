e9df14bb3ef2a6e8c00fe3162e45477f01a01fc6
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-09-17 02:55:53 +0000

    edit generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-
 set -ex
+base=`pwd`
+outdir=$2
+if [ ! -d "$outdir" ]; then
+	mkdir ./$outdir
+fi
 
-# YOUR CODE HERE
+cd $1
+files=(`ls .`)
+numFiles=${#files[@]}
+
+cd $outdir
+for ((i=1; i< $numFiles+1; i++)); do
+	cat $base/template.html > $outdir/post${i}.html
+	subtopic=`head -n 1 $base/$1/${files[i-1]}`
+	subbody=`tail -n 1 $base/$1/${files[i-1]}`
+	sed -i "s/{{title}}/$subtopic/g" post${i}.html
+	sed -i "s/{{body}}/$subbody/g" post${i}.html
+done
+
