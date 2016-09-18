67bfd7215d2f5e7d6ef69968845ac0166bec225e
Lannie Miao <ym232@cornell.edu>
2016-09-18 01:17:15 -0400

    updating generate.sh to fix climate error

--- generate.sh
+++ generate.sh
@@ -3,13 +3,13 @@ set -ex
 
 # YOUR CODE HERE
 ##set file location to be 1st input argument
-inputdir=$1/*
+inputdir="$1/*"
 ## take 2nd argument as the output direcotry
 outputdir=$2
 
 ##check if the output directory is exists or not, and make one if not
 if [ ! -d "$outputdir" ]; then
-  mkdir $outputdir
+  mkdir "$outputdir"
 fi
 
 ##loop through the files in input directory
@@ -19,7 +19,7 @@ do
   ##get the file name, strip out the prefix
   filename=$(basename "$file" .txt)
   newfile=$filename.html
-  titlesub=$(head -1 $file)
-  bodysub=$(tail -1 $file)
-  sed -e 's/{{title}}/'"$titlesub"'/g' -e 's/{{body}}/'"$bodysub"'/g' template.html > $outputdir/$newfile
+  titlesub=$(head -1 "$file")
+  bodysub=$(tail -1 "$file")
+  sed -e 's/{{title}}/'"$titlesub"'/g' -e 's/{{body}}/'"$bodysub"'/g' template.html > "$outputdir/$newfile"
 done
