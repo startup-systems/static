aab626023ec0182e2a24b23f2aa9cb07eeb72a8c
Diego Wolfsdorf <diegow88@github.com>
2016-09-18 06:53:50 +0000

    Remove prints

--- generate.sh
+++ generate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 
 # YOUR CODE HERE
-echo $#
 if [ $# -lt 2 ]
 then
   echo "Expecting two arguments"
@@ -27,12 +26,12 @@ fi
 listfiles=$( ls $src )
 for i in $listfiles
 do
-  echo $i
   name=$( basename "$i" )
   name="${name%.*}"
   title=$( head -n 1 "$src$i" )
   content=$( tail -n +3 "$src$i" )
-  echo "$title|$content"
   cat ./template.html | sed "s/{{title}}/$title/g" | sed "s/{{body}}/$content/g" >> "./$destination$name.html"
 
 done
+
+return 0
