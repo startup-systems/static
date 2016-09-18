a30a245b970006ce1d7c911d185a9815f6c42471
ylynn <wy249@cornell.edu>
2016-09-18 23:39:46 +0000

    assignment submission

--- generate.sh
+++ generate.sh
@@ -11,40 +11,16 @@ if [ ! -d "$2" ]; then
 	mkdir "$2"
 fi
 
-filelist=`ls $1`
+filelist=$1/*
 
 for file in $filelist ; do
+	
 	html=$(echo $file | sed 's/\.txt$/\.html/i')
 
-	echo "<!DOCTYPE html>" > $html
-	echo "<html>" >> $html
-	echo "	<head>" >> $html
-	echo "    <meta charset="utf-8">" >> $html
-
-	count=1
-	while read line ; do
-		if [ $count -eq 1 ]; then
-			l1="<title>{{title}}</title>"
- 			l=$(echo -n $line | sed 's/,/\n/g')
-			l2=$(echo $l1 | sed 's/{{title}}/'$line'/g')
-		
-			echo "    $l2" >> $html 
-			
-
-		elif [ $count -eq 2 ]; then		
-			echo "	</head>" >> $html
-			echo "  </body>" >> $html
+	title=$(head -1 $file)
+	body=$(tail -1 $file)
 	
-		elif [ $count -gt 2 ]; then
-			echo "	  $line" >> $html
-		fi
-		
-		count=`expr $count + 1`		
-
-	done < $1/$file
-
-	echo "	</body>" >> $html
-	echo "</html>" >> $html
+	sed -e 's/{{title}}/'$title'/g' -e 's/{{body}}/'$body'/g' template.html >$html
 	
 	mv $html $2
 done
