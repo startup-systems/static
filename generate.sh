b1e9b23fe71d32c074088fdac4c95dd828573bf7
Ubuntu <charlielabarge1@gmail.com>
2016-09-18 04:50:44 +0000

    basename fix possibly

--- generate.sh
+++ generate.sh
@@ -24,7 +24,7 @@ do
 	title=$(head -n 1 "$file")
 	body=$(tail -n +3 "$file")
 
-	filename=$(basename -s ".txt" "$file") # strip file name of directory and extension
+	filename=$(basename -s .txt "$file") # strip file name of directory and extension
 
 	#create the output file
 	touch "$output_dir/$filename.html"
