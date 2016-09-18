9afe63322feb6a05523e78bdaf45a4962ddb9642
Renee <rle87@cornell.edu>
2016-09-18 18:36:04 +0000

    Script passes tests

--- generate.sh
+++ generate.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
 set -ex
+input=$1
+lastchar="${input: -1}"
+if [ "$lastchar" != "/" ]
+   then 
+       input="$input/"
+fi
 
-# YOUR CODE HERE
+for file in $input*
+do
+     
+    fbname=$(basename "$file"| cut -d. -f1)
+    output=$2
+    mkdir -p $output
+    lastchar="${output: -1}"
+    if [ "$lastchar" != "/" ]
+	then 
+           output="$output/"
+    fi
+    title=$(head --lines=1 $file)
+    body=$(tail --lines=1 $file)
+    sed "s/{{title}}/$title/g" template.html > temp.html
+    sed "s/{{body}}/$body/g" temp.html > $output$fbname.html
+done
+rm temp.html
