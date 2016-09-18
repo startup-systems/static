1c9076055928c9d93441f694580223904051c243
Renee <rle87@cornell.edu>
2016-09-18 18:45:49 +0000

    Added quotes around variables

--- generate.sh
+++ generate.sh
@@ -13,15 +13,15 @@ do
      
     fbname=$(basename "$file"| cut -d. -f1)
     output=$2
-    mkdir -p $output
+    mkdir -p "$output"
     lastchar="${output: -1}"
     if [ "$lastchar" != "/" ]
 	then 
            output="$output/"
     fi
-    title=$(head --lines=1 $file)
-    body=$(tail --lines=1 $file)
+    title=$(head --lines=1 "$file")
+    body=$(tail --lines=1 "$file")
     sed "s/{{title}}/$title/g" template.html > temp.html
-    sed "s/{{body}}/$body/g" temp.html > $output$fbname.html
+    sed "s/{{body}}/$body/g" temp.html > "$output$fbname".html
 done
 rm temp.html
