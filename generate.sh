51365e34d8c443a6bdd1e728a47855d719ea970d
David Cheng <ddc88@cornell.edu>
2016-09-19 23:41:06 +0000

    fixing stupid complaint about globbing

--- generate.sh
+++ generate.sh
@@ -15,12 +15,17 @@ do
 	# grab first line of file
 	postTitle="$(head -n 1 "$file")"
 	
+
 	# grab rest of file, replacing new lines with \a character
 	postBody="$(tail -n +3 "$file" | tr "\n" "\a")"
 
 	# swap for extra credit
 	# postBodyEdited="$(tail -n +3 "$file" | tr "\n" "\a")"
-
+	# regex for html
+	# https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
+	# i tried
+	
 	# insert template contents (use sed to swap out title and body, then tr to swap out \a's with new lines)
 	sed "s/{{title}}/$postTitle/g" "template.html" | sed "s|{{body}}|$postBody|" | tr "\a" "\n" > $OUTPUT_DESTINATION/$(basename $file .txt).html
 done
+
