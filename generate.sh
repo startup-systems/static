b40ee3e87180960e52ca280df6aa81b291a228cf
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2016-09-19 22:43:12 +0000

    done

--- generate.sh
+++ generate.sh
@@ -6,15 +6,15 @@ if [ ! -d "$outdir" ]; then
 	mkdir -p "$outdir"
 fi
 
-files=(`ls $1`)
+files=($(ls $1))
 numFiles=${#files[@]}
 
 for ((i=0; i < numFiles; i++)); do
 	outfile=$(basename "${files[i]}")
 	outfile=${outfile%.*}
 	cat template.html > "$outdir/$outfile.html"
-	subtopic=`head -n 1 "$inputpath/${files[i]}"`
-	subbody=`tail -n 1 "$inputpath/${files[i]}"`
+	subtopic=$(head -n 1 "$inputpath/${files[i]}")
+	subbody=$(tail -n 1 "$inputpath/${files[i]}")
 	sed -i "s@{{title}}@$subtopic@g" "$outdir/$outfile.html"
 	sed -i "s@{{body}}@$subbody@g" "$outdir/$outfile.html"
 done
