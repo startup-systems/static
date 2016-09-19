b0bc57e208ad643a08bf59bba3e9d1863bcc5868
ylynn <wy249@cornell.edu>
2016-09-19 00:33:17 +0000

    sub

--- generate.sh
+++ generate.sh
@@ -2,22 +2,18 @@
 
 set -ex
 
-if [ $# -eq 0 ] ; then
-	echo "USAGE: $(basename $0) input_dir output_dir"
-	exit 1
-fi
-
 if [ ! -d "$2" ]; then
 	mkdir "$2"
 fi
 
-for file in $1/*.txt ; do
+input="$1/*"
+for file in $input ; do
 	
-	name="$(basename "$file" .txt)"
+	name=$(basename "$file" .txt)
 
 	title=$(head -1 "$file")
 	body=$(tail -1 "$file")
 	
-	sed -e "s/{{title}}/$title/g" -e "s/{{body}}/$body/g" template.html > $2/$name.html
+	sed -e 's/{{title}}/'"$title"'/g' -e 's/{{body}}/'"$body"'/g' template.html > "$2/$name.html"
 	
 done
