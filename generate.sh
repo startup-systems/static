67af6073d3adaa82f273089ce10556a429de47d0
Zephyr Liu <youxiang518@gmail.com>
2016-09-19 00:24:46 -0400

    re-init
    re-submit for the newest version of pytest

--- generate.sh
+++ generate.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-set -ex
+# set -ex
 
 # YOUR CODE HERE
+if (( $# != 2 )); then
+    echo "Illegal Number of Parameters"
+    echo "Synopsis"
+    echo "./generate.sh [-input] [-output]"
+    exit
+fi
+
+count=0
+mkdir -p $2
+for f in "$1"/*.txt
+do
+    echo parsering \file $(basename $f)
+    let count=count+1
+    outfile="$2"/$(basename $f .txt).html
+    sed "s/{{title}}/$(head -1 $f)/;s/{{body}}/$(tail -n +3 $f)/" ./template.html > $outfile
+done
+echo created $count files at $2
