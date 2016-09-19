4064f31eb351e5f885ef46f69b01fd8eb3cf5799
travis holt <trvslhlt@gmail.com>
2016-09-19 17:51:50 +0000

    Majority of script in place

--- generate.sh
+++ generate.sh
@@ -3,3 +3,39 @@
 set -ex
 
 # YOUR CODE HERE
+INPUT_DIRECTORY=$1
+OUTPUT_DIRECTORY=$2
+
+#function to inject title and body into html template
+populate_template() {
+        title=$1
+        body=$2
+        html_template_filename="./template.html"
+        html_template=`sudo cat $html_template_filename`
+        html_with_title=`sed -e "s/{{title}}/${title}/g" <<< $html_template`
+        html=`sed -e "s/{{body}}/${body}/g" <<< $html_with_title`
+        echo $html
+}
+
+# Prepare the output directory
+if [ -d "$OUTPUT_DIRECTORY" ]; then
+  # Control will enter here if $DIRECTORY exists.
+        echo "Removing files from output directory"
+        `sudo rm -rf "$OUTPUT_DIRECTORY"/*`
+else
+        echo "Making output directory $OUTPUT_DIRECTORY"
+        `sudo mkdir -p $OUTPUT_DIRECTORY`
+fi
+
+# Iterate over the example articles
+for txt_filepath in $INPUT_DIRECTORY/*; do
+	echo $txt_filepath
+        text=`cat $txt_filepath`
+        title=`echo "$text" | head -n 1`
+        body=`echo "$text" | tail -1`
+	html=$(populate_template "$title" "$body")
+        txt_filename=`basename $txt_filepath`
+        html_filename=`sed -e "s/txt/html/g" <<< $txt_filename`
+        html_filepath=$OUTPUT_DIRECTORY'/'$html_filename
+        echo $html | sudo tee -a $html_filepath
+done
