1c3ed42023ffafe2598e591773dfa501f731f2e4
gaobocn <bg447@cornell.edu>
2016-09-19 14:24:41 +0000

    generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,22 @@
-#!/bin/bash
-
+#/bin/bash
 set -ex
 
 # YOUR CODE HERE
+
+tempfile=template.html
+
+indir=$1
+outdir=$2
+
+if [ ! -d "$outdir" ]; then
+  mkdir $outdir
+fi
+
+for file in $indir/*
+do
+  fname=$(basename "$file" .txt)
+  newfile=$fname.html
+  txttitle=$(head -1 $file)
+  txtbody=$(tail -1 $file)
+  sed -e 's/{{title}}/'"$txttitle"'/g' -e 's/{{body}}/'"$txtbody"'/g' $tempfile > $outdir/$newfile
+done
