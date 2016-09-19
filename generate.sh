f40a23254a065aa5c590fb30e05882b7f7521fdc
David Cheng <ddc88@cornell.edu>
2016-09-19 22:36:48 +0000

    fixing stupid complaint about globbing

--- generate.sh
+++ generate.sh
@@ -13,10 +13,10 @@ mkdir -p "$OUTPUT_DESTINATION"
 for file in $INPUT_SOURCE/*.txt
 do
 	# grab first line of file
-	postTitle=$(head -n 1 "$file")
+	postTitle="$(head -n 1 "$file")"
 	
 	# grab rest of file, replacing new lines with \a character
-	postBody=$(tail -n +3 "$file" | tr "\n" "\a")
+	postBody="$(tail -n +3 "$file" | tr "\n" "\a")"
 
 	# insert template contents (use sed to swap out title and body, then tr to swap out \a's with new lines)
 	cat "template.html" | sed "s/{{title}}/$postTitle/g" | sed "s|{{body}}|$postBody|" | tr "\a" "\n" > $OUTPUT_DESTINATION/$(basename $file .txt).html
