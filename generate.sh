b25c163c84e7ed1f1f37ace839787140ff91bbfd
Ubuntu <jillian.sue3@gmail.com>
2016-09-19 22:21:13 +0000

    fourth try

--- generate.sh
+++ generate.sh
@@ -5,28 +5,28 @@ set -ex
 # YOUR CODE HERE
 #python3 generate.py $1 $2
 
-inputFolder = $1
-outputFolder = $2
+inputFolder=$1
+outputFolder=$2
 
 if [ ! -d $outputFolder ]; then
     mkdir $outputFolder
 fi
 
 for file in $inputFolder*; do
-    fullFilePath = $file
-    baseFileName = 'basename $fullFilePath'
-    baseFileName = 'echo $baseFileName | cut -f 1 -d .'
+    fullFilePath=$file
+    baseFileName='basename $fullFilePath'
+    baseFileName='echo $baseFileName | cut -f 1 -d .'
 
-    title = ''
-    body = ''
-    flag = 0
+    title=''
+    body=''
+    flag=0
     while read line; do 
         if [ $(echo $line | wc -c) -eq 1 ]; then 
-            flag = 1
+            flag=1
         elif [ $flag -eq 0 ]; then
-            title = "${title} ${line}"
+            title="${title} ${line}"
         else
-            body = "${body}""${line}"$'<br>'
+            body="${body}""${line}"$'<br>'
         fi
     done <$fullFilePath 
 
@@ -35,5 +35,4 @@ for file in $inputFolder*; do
     sed -i 0 -e "s/{{title}}/${title/g" -e "s/{{body}}/$body/g" ${outputFolder}${baseFileName}.html
 
     rm "${outputFolder}${baseFileName}.html0"
-
 done
