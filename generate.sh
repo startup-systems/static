cd30fd937e6aecbb01283e537ce20ee7bcf2b025
David Cheng <ddc88@cornell.edu>
2016-09-19 21:19:09 +0000

    updated generate.sh to build static sites

--- generate.sh
+++ generate.sh
@@ -2,4 +2,22 @@
 
 set -ex
 
-# YOUR CODE HERE
+# set variables
+INPUT_SOURCE=$1
+OUTPUT_DESTINATION=$2
+
+# recursively creates folder structure for output
+mkdir -p $OUTPUT_DESTINATION
+
+# loop through files ending in .txt in new output folder structure
+for file in $(find $INPUT_SOURCE -type f -name "*.txt")
+do
+	# grab first line of file
+	postTitle=$(head -n 1 $file)
+	
+	# grab rest of file, replacing new lines with \a character
+	postBody=$(tail -n +3 $file | tr '\n' '\a')
+
+	# insert template contents (use sed to swap out title and body, then tr to swap out \a's with new lines)
+	cat template.html | sed "s/{{title}}/$postTitle/g" | sed "s|{{body}}|$postBody|" | tr '\a' '\n' > $OUTPUT_DESTINATION/$(basename $file .txt).html
+done
