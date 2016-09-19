2895c0ac54284e999973a268425d0de9d8f6d16a
ylynn <wy249@cornell.edu>
2016-09-19 15:11:07 +0000

    re_sub

--- generate.sh
+++ generate.sh
@@ -12,5 +12,5 @@ for file in $input ; do
  name=$(basename "$file" .txt)
  title=$(head -1 "$file")
  body=$(tail -1 "$file")
- sed -e 's/{{title}}/'"$title"'/g' -e 's/{{body}}/'"$body"'/g' template.html > "$2/$name.html"
+ sed "s/{{title}}/$title/;s/{{body}}/$body/" template.html > "$2/$name.html"
 done
