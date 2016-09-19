42be736b7c8f094b2652e1dfc05a7008b5694547
Evan Kesten <evan@challengage.com>
2016-09-19 18:22:29 -0400

    fixing some of the shellcheck errors

--- generate.sh
+++ generate.sh
@@ -3,22 +3,22 @@
 set -ex
 
 STARTDIR=$(pwd)
-cd $1
+cd "$1"
 INPUTDIR=$(pwd)
 cd -
-mkdir -p $2
-cd $2
+mkdir -p "$2"
+cd "$2"
 OUTPUTDIR=$(pwd)
 cd -
 
 cd $INPUTDIR
-rsync -Rv $(find . -name '*.txt') $OUTPUTDIR
+rsync -Rv $(find . -name '*.txt') "$OUTPUTDIR"
 cd $STARTDIR
 
-for FILENAME in $(find $OUTPUTDIR -name '*.txt'); do
-	cat template.html > $FILENAME.html
-	TITLE=$(head -n 1 $FILENAME)
-	BODY=$(tail -n +3 $FILENAME)
+for FILENAME in $(find "$OUTPUTDIR" -name '*.txt'); do
+	cat template.html > "$FILENAME.html"
+	TITLE=$(head -n 1 "$FILENAME")
+	BODY=$(tail -n +3 "$FILENAME")
 	echo "$BODY" > tmp_body
 	sed -i.bak "s/^[^< ].*/<p>&<\/p>/g" tmp_body
 	sed -i.bak "s/\\\/\\\\\\\\/g" tmp_body
@@ -30,19 +30,19 @@ for FILENAME in $(find $OUTPUTDIR -name '*.txt'); do
 	rm tmp_body
 	rm tmp_body.bak
 	
-	sed -i.bak "s/{{title}}/$TITLE/g" $FILENAME.html
-	sed -i.bak "s/{{body}}/$BODY/g" $FILENAME.html
+	sed -i.bak "s/{{title}}/$TITLE/g" "$FILENAME.html"
+	sed -i.bak "s/{{body}}/$BODY/g" "$FILENAME.html"
 
-	sed -i.bak "s/https*:\/\/[^.]*\.[^ \r\n\.]*/<a href=\"&\">&<\/a>/g" $FILENAME.html
+	sed -i.bak "s/https*:\/\/[^.]*\.[^ \r\n\.]*/<a href=\"&\">&<\/a>/g" "$FILENAME.html"
 
-	sed -i.bak "s/<\\/p> /&\n/g" $FILENAME.html
+	sed -i.bak "s/<\\/p> /&\n/g" "$FILENAME.html"
 
-	sed -i.bak "s/<\\/p>/\n&/g" $FILENAME.html
+	sed -i.bak "s/<\\/p>/\n&/g" "$FILENAME.html"
 
-	sed -i.bak "s/<p>/&\n/g" $FILENAME.html
+	sed -i.bak "s/<p>/&\n/g" "$FILENAME.html"
 
-	rm $FILENAME
-	rm $FILENAME.html.bak	
+	rm "$FILENAME"
+	rm "$FILENAME.html.bak"	
 done
 
-find $OUTPUTDIR -name '*.txt.html' -exec bash -c 'mv "$1" "${1%.txt.html}".html' - '{}' \;
+find "$OUTPUTDIR" -name '*.txt.html' -exec bash -c 'mv "$1" "${1%.txt.html}".html' - '{}' \;
