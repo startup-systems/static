55ace46177bd01955063bb381fbdb480d7507f1e
ylynn <wy249@cornell.edu>
2016-09-19 18:13:44 +0000

    re-sub

--- generate.sh
+++ generate.sh
@@ -4,17 +4,13 @@ set -ex
 
 output="$2"
 if [ ! -d "$output" ]; then
-	mkdir -p "$2"
+  mkdir -p "$2"
 fi
 
 input="$1/*"
 for file in $input ; do
-	
-	name="$(basename "$file" .txt)"
-
-	title="$(head "$file" -n 1)"
-	body="$(tail "$file" -n 1)"
-	
-	sed "s/{{title}}/$title/;s/{{body}}/$body/" template.html > $2/$name.html
-	
+  name="$(basename "$file" .txt)"
+  title="$(head "$file" -n 1)"
+  body="$(tail "$file" -n 1)"
+  sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$body"'#g' template.html > $2/$name.html
 done
