975b6fff9b8ff573134e624072aaff6c2f4f307d
gaobocn <bg447@cornell.edu>
2016-09-19 19:58:22 +0000

    codeclimate

--- generate.sh
+++ generate.sh
@@ -13,11 +13,11 @@ if [ ! -d "$outdir" ]; then
   mkdir "$outdir"
 fi
 
-for file in "$indir"/*
+for file in "$indir/*"
 do
   fname=$(basename "$file" .txt)
   newfile="$fname".html
   txttitle=$(head -1 "$file")
   txtbody=$(tail -1 "$file")
-  sed -e 's/{{title}}/'"$txttitle"'/g' -e 's#{{body}}#'"$txtbody"'#g' "$tempfile" > "$outdir$newfile"
+  sed -e 's/{{title}}/'"$txttitle"'/g' -e 's/{{body}}/'"$txtbody"'/g' "$tempfile" > "$outdir$newfile"
 done
