7ce9a2f0d0d1a3a2bdabc4707accb0b6dbd3f96a
AltmerX <sabrina5426@live.com>
2016-09-20 05:52:40 +0000

    fixed unterminated sed: try it again

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 set -ex
 for filename in $1/*.txt; do
     title="$(head -n 1 "$filename")"
-    body="$(sed -n '3,$p' "$filename")"
+    body="$(tail -n 1 "$filename")"
     htmlfile="$(basename "$filename" .txt).html"
     mkdir -p "$2"
     echo "$filename" | sed -e 's#{{body}}#'"$body"'#g' -e 's#{{title}}#'"$title"'#g' "template.html" > "$2"/"$htmlfile"
