9988b787f35f7915110aed77ffffcef7859cfb55
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-09-19 20:21:41 -0400

    finish

--- generate.sh
+++ generate.sh
@@ -3,3 +3,18 @@
 set -ex
 
 # YOUR CODE HERE
+file_output="$2"
+if [ ! -d "$file_output" ]
+then
+mkdir "/$file_output"
+fi
+
+file_input="$1/*"
+
+for files in $file_input
+do
+title=$(head -n 1 $files)
+content=$(tail -n +3 $files)
+output_name=$(basename "$files" .txt).html
+sed -e 's/{{title}}/'"$title"'/g' -e 's/{{body}}/'"$content"'/g' template.html > "$2/$output_name"
+done
