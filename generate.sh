4bbd35067ec5a1ac546c9df3aee952716bb8ecb8
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-09-20 19:36:03 -0400

    first homework

--- generate.sh
+++ generate.sh
@@ -2,4 +2,9 @@
 
 set -ex
 
-# YOUR CODE HERE
+for i in "$1"/*;
+do
+
+mkdir -p "$2"
+sed "s/{{title}}/$(head -1 $i)/g" template.html | sed "s/{{body}}/$(tail +3 $i | sed -e ':a' -e '$!{' -e 'N' -e 'ba' -e '}' -e 's/\n/^/g' | sed 's/\//~/g')/g" | tr '~' '/' | tr '^' '\n' > $(echo "$2"/$(basename $i | cut -d '.' -f1).html) ;
+done
