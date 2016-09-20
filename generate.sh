160a9df2009e42c4384059eefaca24c2aaf4060c
mw866 <mw866@cornell.edu>
2016-09-20 22:41:41 +0800

    Commit while runnning pytests

--- generate.sh
+++ generate.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-set -x #print commands
+#set -x #print commands
 set -e #exit upon errors
 echo "[Running the script..]"
-for input_file in "$1"/*;do
-	echo "<$input_file>"
-	output_file=$($2/$(basename "$input_file").html)
-	echo "$output_file" 
-	mkdir $2	
-	touch output_file
+for input_file_path in "$1"/*;do
+	echo "INPUT FILE PATH: $input_file_path"
+	input_file_basename=$(basename "$input_file_path")
+	echo "INPUT FILE BASENAME: $input_file_basename" 
+	input_file_basename_without_extension=$(basename "$input_file_path" .txt)
+	echo "INPT FILE BASENAME WITHOUT EXTENSION: $input_file_basename_without_extension"
+	output_file_path=$2/$input_file_basename_without_extension.html
+
+	echo "OUTPUT FILE PATH:$output_file_path"
+	mkdir -p $2	
+	touch $output_file_path
 	
-	title=$(cat "$input_file" | sed -n '1p')
+	title=$(cat "$input_file_path" | sed -n '1p')
 	#echo "$title"
-	body=$(cat "$input_file" | sed -n 2~1p | sed -e 'i <p>' -e 'a </p>')
+	body=$(cat "$input_file_path" | sed -n 2~1p | sed -e 'i <p>' -e 'a </p>')
 	#echo "$body"
 
 
-	echo "<!DOCTYPE html>
-	<html>
-  	<head>
-    		<meta charset="utf-8">
+	echo "
+<!DOCTYPE html>
+<html>
+ 	<head>
+  		<meta charset="utf-8">
     		<title>$title</title>
   	</head>
   	<body>
     		$body
   	</body>
-	</html>">>"$output_file"
+</html>">"$output_file_path"
 done
 
