3c56a210d1edcf060136237b9f1864a371f6cf90
mw866 <mw866@cornell.edu>
2016-09-20 08:22:52 +0800

    Initial commit Updated generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-set -ex
+#set -x #print commands
+set -e #exit upon errors
+echo "[Running the script..]"
+for input_file in "$1"/*;do
+	echo "<$input_file>"
+	output_file=$(basename "$input_file")
+	echo "$output_file" 
 
-# YOUR CODE HERE
+	title=$(cat "$input_file" | sed -n '1p')
+	#echo "$title"
+	body=$(cat "$input_file" | sed -n 2~1p | sed -e 'i <p>' -e 'a </p>')
+	#echo "$body"
+
+
+	echo "<!DOCTYPE html>
+	<html>
+  	<head>
+    		<meta charset="utf-8">
+    		<title>$title</title>
+  	</head>
+  	<body>
+    		$body
+  	</body>
+	</html>">>"./$output_file.html"
+done
+
