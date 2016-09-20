fc307dc97ba66a14abe318760f610710a6a7347a
rwg58 <rwg58@mail.missouri.edu>
2016-09-20 14:32:58 -0400

    modify generate

--- generate.sh
+++ generate.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+outdirect=$2
+
+if [ ! -d "$outdirect" ];then
+mkdir $outdirect
+fi
+
 for file in "$1"/*
 do
-subfile=$(basename $file .txt)
-htmlfile=$subfile.html
+	subfile=$(basename $file .txt)
+	htmlfile=$subfile.html
 
-mytitle=$(head -n 1 $file)
-mybody=$(tail -n+3 $file)
+	mytitle=$(head -n 1 $file)
+	mybody=$(tail -n+3 $file)
 
-sed -e 's/{{title}}/'"$mytitle"'/' -e 's/{{body}}/'"$mybody"'/' template.html >> $2/$htmlfile
-
+	sed -e 's/{{title}}/'"$mytitle"'/' -e 's/{{body}}/'"$mybody"'/' template.html >> $outdirect/$htmlfile
 done
+
