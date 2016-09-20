9791415d1eadb5f6bf5904609934b78767bbfc17
vagrant <utsavvakil@gmail.com>
2016-09-20 18:21:14 +0000

    Added generate.sh file

--- generate.sh
+++ generate.sh
@@ -3,3 +3,18 @@
 set -ex
 
 # YOUR CODE HERE
+outputDirectory=$2
+template=$(<template.html)
+
+for file in $1/*
+do
+        while read p; do
+                if [ ! -d $outputDirectory ]; then
+                        mkdir -p "$outputDirectory"
+                fi
+                outputFile=$(basename "$file" ".txt")
+                echo $template > $outputDirectory"/"$outputFile".html"
+                sed -i -e "s/{{title}}/$( head -n 1 $file)/g" $outputDirectory"/"$outputFile".html"
+                sed -i -e "s/{{body}}/$( tail -n +3 $file)/g" $outputDirectory"/"$outputFile".html"
+        done<$file
+done
