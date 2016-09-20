fcb33bcd2b422bce4781a98843752ea24d6e8c2f
Ubuntu <jillian.sue3@gmail.com>
2016-09-20 03:23:05 +0000

    hopefully last try

--- generate.sh
+++ generate.sh
@@ -14,25 +14,30 @@ fi
 
 for file in $inputFolder*; do
     fullFilePath=$file
-    baseFileName=$(basename $fullFilePath)
-    baseFileName='echo $baseFileName | cut -f 1 -d .'
+    baseFileName=`basename $fullFilePath`
+    baseFileName=`echo $baseFileName | cut -f 1 -d .`
 
     title=''
-    body=''
+    body='<p>'
     flag=0
     while read line; do 
-        if [ $(echo $line | wc -c) -eq 1 ]; then 
+        if [ $(echo $line | wc -c) -eq 1 ] && [ $flag -eq 0 ]; then 
             flag=1
         elif [ $flag -eq 0 ]; then
             title="${title} ${line}"
         else
-            body="${body}""${line}"$'<br>'
+            if [ $(echo $line | wc -c) -eq 1 ];then
+                body="${body}<\/p><p>${line}<br>"
+            else
+                body="${body}""${line}"$'<br>'
+            fi
         fi
     done <$fullFilePath 
 
-    cp template.html ${outputFolder}${baseFileName}.html
+    body="${body}<\/p>"
+    cp template.html "${outputFolder}/${baseFileName}.html"
     
-    sed -i 0 -e "s/{{title}}/$title/g" -e "s/{{body}}/$body/g" ${outputFolder}${baseFileName}.html
-
-    rm "${outputFolder}${baseFileName}.html0"
+    sed -i -e "s/{{title}}/$title/g" -e "s/{{body}}/$body/g" "${outputFolder}/${baseFileName}.html"
+    #sed -i -e "s/{https_ }/dd
+    #rm "${outputFolder}/${baseFileName}.html0"
 done
