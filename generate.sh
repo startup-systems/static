b45bf8b7751bfa43ae5ee2e7bc1d2fe23e6e73dd
42lince <42.duxiaoxi@gmail.com>
2016-09-20 19:13:57 +0000

    submission

--- generate.sh
+++ generate.sh
@@ -2,4 +2,25 @@
 
 set -ex
 
-# YOUR CODE HERE
+inputdir=$1/*
+outputdir=$2
+if [ ! -d "$outputdir" ]; then
+    mkdir -p $outputdir
+fi
+
+template=~/static/template.html
+for i in $inputdir; do
+    lines=$(sed -n $= $i)
+    title=$(head -1 $i)
+    body=$(tail -n $(($lines-2)) $i)
+    
+    oldfilename=$(basename $i)
+    newfilename=$(echo $oldfilename | sed s/txt/html/)
+
+    sed "s/{{title}}/$title/g" $template > $outputdir/$newfilename
+    sed -i "s/{{body}}/$body/g" $outputdir/$newfilename
+    
+    #extra credit
+	
+done
+
