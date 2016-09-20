1023a670dd5da401427096a3886a2d9656e12ed2
vagrant <abhinandam@gmail.com>
2016-09-20 16:55:34 +0000

    upgrade code quality

--- generate.sh
+++ generate.sh
@@ -6,17 +6,17 @@ outputDirectory=$2;
 
 # create output directory if doesn't exist
 if [[ ! -d $outputDirectory ]]; then
-  mkdir -p $outputDirectory;
+  mkdir -p "$outputDirectory";
 fi
 
 # loop through input directory
 for file in $inputDirectory/*.txt; do
   # convert extension to html
-  fileName="$(basename ${file%.*})".html;
+  fileName=""$(basename ${file%.*})".html";
 
   # extract title and body
-  title=$(head -n 1 $file);
-  body=$(tail -n +3 $file);
+  title="$(head -n 1 $file)";
+  body="$(tail -n +3 $file)";
 
   # copy template to corresponding html file
   cat template.html > "$outputDirectory/$fileName";
