2c40d64f61e2ed12bab826688add7eb830937d78
rwg58 <rwg58@mail.missouri.edu>
2016-09-20 14:42:09 -0400

    second push

--- generate.sh
+++ generate.sh
@@ -3,17 +3,17 @@
 outdirect=$2
 
 if [ ! -d "$outdirect" ];then
-mkdir $outdirect
+mkdir -p "$outdirect"
 fi
 
 for file in "$1"/*
 do
-	subfile=$(basename $file .txt)
+	subfile=$(basename "$file" .txt)
 	htmlfile=$subfile.html
 
-	mytitle=$(head -n 1 $file)
-	mybody=$(tail -n+3 $file)
+	mytitle=$(head -n 1 "$file")
+	mybody=$(tail -n+3 "$file")
 
-	sed -e 's/{{title}}/'"$mytitle"'/' -e 's/{{body}}/'"$mybody"'/' template.html >> $outdirect/$htmlfile
+	sed -e 's/{{title}}/'"$mytitle"'/' -e 's/{{body}}/'"$mybody"'/' template.html >> "$outdirect/$htmlfile"
 done
 
