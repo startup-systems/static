49736eb678059b20215903ed2e0a2062f9a26e6d
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-09-20 00:42:07 -0400

    modify tail

--- generate.sh
+++ generate.sh
@@ -16,6 +16,6 @@ do
   output_name=$(basename "$files" .txt)
   new_name=$output_name.html
   title=$(head -1 $files)
-  content=$(tail -n +3 $files)
+  content=$(tail -n 1 $files)
   sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$content"'#g' template.html > "$file_output/$new_name"
 done
