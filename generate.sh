58bbfd83701664c116cf14266b705aec4be2949a
Yan-J <yan_j1213@126.com>
2016-09-21 06:04:56 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,51 +1,38 @@
 #!/bin/bash
 
+
 set -ex
 
 dir_in="$1/*"
 dir_out="$2"
 
-
 if [ ! -d "$dir_out" ]; then
 	mkdir -p "$dir_out"
 fi
 
-function wt_head(){
-	echo -e "<!DOCTYPE html>
-<html>
-  	<head>
-    	<meta charset="utf-8">"
-}
-
-
-function wt_title(){
-		echo -e "<title>"
-		sed -n '1'p ${file_in}
-		echo -e "</title>"
-	
-}
-
-function wt_body(){
-	echo -e "	</head>
- 	<body>"
-  	sed -n '3,$'p ${file_in}
-}
-
-function wt_end(){
-	echo -e "	</body>
-</html>"
+function wt_file(){
+	echo -e "<!DOCTYPE html>"
+	echo -e "<html>"
+	echo -e "	<head>"
+  	echo -e "		<meta charset="utf-8">"   	
+	echo -e "		<title>"
+	sed -n '1p' ${file_in}
+	echo -e "		</title>"	
+	echo -e "	</head>"
+ 	echo -e "	<body>"
+  	sed -n '3,$p' ${file_in}
+	echo -e "	</body>"
+	echo -e "</html>"
 }
 
 
 function convert(){
 	for file_in in $dir_in; do 
 	filename="$(basename "$file_in" .txt)"
-	echo $filename
-	wt_head > "$dir_out/$filename.html"
-	wt_title >> "$dir_out/$filename.html"
-	wt_body >> "$dir_out/$filename.html"
-	wt_end >> "$dir_out/$filename.html"	
+	wt_file > "$dir_out/$filename.html"	
+
 done
+
 }
 
 convert
