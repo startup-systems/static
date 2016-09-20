93f7198db96573fd4fffe6cf2e6f840433ceb4fe
bluenight1994 <1137361494@qq.com>
2016-09-19 23:16:19 -0400

    first commit

--- generate.sh
+++ generate.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 
 # YOUR CODE HERE
+SRC=$1
+DST=$2
+mkdir "$DST"
+
+for filename in $SRC/*.txt
+do
+	tmp=`basename $filename .txt`
+	output="$DST"/"$tmp".html 
+	sed "s/{{title}}/$(head -1 "$filename")/;s/{{body}}/$(tail -n 1 "$filename")/" <./template.html > "$output"
+done 
