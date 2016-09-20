770ca2c75b5cbe1b621bd0bbe0da15c71d240163
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-09-19 23:11:09 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,8 +6,7 @@ set -ex
 file_output=$2
 if [ ! -d "$file_output" ]
 then
-rm -rf "${file_output:?}"/*
-mkdir -p "$file_output"
+mkdir "$file_output"
 fi
 
 file_input="$1/*"
@@ -15,7 +14,7 @@ file_input="$1/*"
 for files in $file_input
 do
 title=$(head -n 1 $files)
-content=$(tail -n +2 $files)
+content=$(tail -n +3 $files)
 output_name=$(basename "$files" .txt).html
-sed -e 's/{{title}}/'"$title"'/g' -e 's/{{body}}/'"$content"'/g' template.html > "$2/$output_name"
+sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$content"'#g' template.html > "$2/$output_name"
 done
