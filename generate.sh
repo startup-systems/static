3c0969dc4d83ab91d066fdf62e869bc1499385c1
Ubuntu <jillian.sue3@gmail.com>
2016-09-20 18:50:16 +0000

    again again

--- generate.sh
+++ generate.sh
@@ -18,13 +18,13 @@ for file in $inputFolder*; do
     #baseFileName=`echo $baseFileName | cut -f 1 -d .`
 
     title=''
-    body='<p>'
+    #body='<p>'
     flag=0
     while read line; do 
         if [ $(echo $line | wc -c) -eq 1 ] && [ $flag -eq 0 ]; then 
             flag=1
         elif [ $flag -eq 0 ]; then
-            title="${title} ${line}"
+            title="${line}"
         else
             if [ $(echo $line | wc -c) -eq 1 ];then
                 body="${body}<p>${line}</p>"
