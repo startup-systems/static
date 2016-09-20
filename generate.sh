aba5b49b9471889767d691233b7929a13182d5f9
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-09-20 00:28:27 -0400

    modify input

--- generate.sh
+++ generate.sh
@@ -4,17 +4,18 @@ set -ex
 
 # YOUR CODE HERE
 file_output=$2
-if [ ! -d "$file_output" ]; then
+if [ ! -d "$file_output" ] 
+then
   mkdir -p "$file_output"
 fi
 
-file_input="$1/*"
+file_input="$1"
 
-for files in $file_input
+for files in "$file_input"/*
 do
-  output_name="$(basename "$files" .txt)"
+  output_name=$(basename "$files" .txt)
   new_name=$output_name.html
-  title="$(head -1 $files)"
-  content="$(tail -n +3 $files)"
+  title=$(head -1 $files)
+  content=$(tail -n +3 $files)
   sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$content"'#g' template.html > "$file_output/$new_name"
 done
