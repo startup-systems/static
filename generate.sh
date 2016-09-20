56345897e4826b8bc6d59d38962ad19194aa7c25
Mengjiao ZHU <mengjiao0326@gmail.com>
2016-09-20 00:07:27 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ for files in $file_input
 do
   output_name=$(basename "$files" .txt)
   new_name=$output_name.html
-  title=$(head -1 "$files")
-  content=$(tail -n +3 "$files")
+  title=$(head -1 $files)
+  content=$(tail -n +3 $files)
 sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$content"'#g' template.html > "$file_output/$new_name"
 done
