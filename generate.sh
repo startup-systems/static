00845e5141bbc20f49696fb5036899efdfebcfd9
Ubuntu <jillian.sue3@gmail.com>
2016-09-20 22:06:37 +0000

    tenth try

--- generate.sh
+++ generate.sh
@@ -2,37 +2,37 @@
 
 set -ex
 
-# YOUR CODE HERE
+#YOUR CODE HERE
 #python3 generate.py $1 $2
 
 inputFolder=$1
 outputFolder=$2
 
-if [ ! -d $outputFolder ]; then
-    mkdir $outputFolder
+if [ ! -d "$outputFolder" ]; then
+    mkdir -p "$outputFolder"
 fi
 
 for file in $inputFolder*; do
     fullFilePath=$file
-    baseFileName=`basename $fullFilePath .txt`
+    baseFileName=`basename "$fullFilePath" .txt`
     #baseFileName=`echo $baseFileName | cut -f 1 -d .`
 
     title=''
     #body='<p>'
     flag=0
-    while read line; do 
-        if [ $(echo $line | wc -c) -eq 1 ] && [ $flag -eq 0 ]; then 
+    while read -r line; do 
+        if [ $(echo "$line" | wc -c) -eq 1 ] && [ "$flag" -eq 0 ]; then 
             flag=1
         elif [ $flag -eq 0 ]; then
             title="${line}"
         else
-            if [ $(echo $line | wc -c) -eq 1 ];then
+            if [ $(echo "$line" | wc -c) -eq 1 ];then
                 body="${body}<p>${line}</p>"
             else
-                body="${body}""${line}"
+                body="${body}${line}"
             fi
         fi
-    done <$fullFilePath 
+    done <"$fullFilePath" 
 
     body="${body}"
     cp template.html "${outputFolder}/${baseFileName}.html"
