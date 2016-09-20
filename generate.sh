360e630081f3dc5c18c057b932f9f75abc5fbea8
Federico Zarfati <fz234@cornell.edu>
2016-09-20 11:13:47 -0400

    First try

--- generate.sh
+++ generate.sh
@@ -2,4 +2,22 @@
 
 set -ex
 
-# YOUR CODE HERE
+if [ -d "$1" ]; then
+  if [ ! -d "$2" ]; then
+    mkdir -pv $2    
+  fi
+
+
+
+for file in "$1"/*.txt
+  do
+
+    title="$(cat $file | head -n 1)"
+
+    body="$(cat $file | tail -n 1)"
+
+    base="$(basename $file .txt)"
+
+    sed "s/{{title}}/$title/g;s/{{body}}/$body/g" template.html >> $2/$base.html
+    done
+fi
