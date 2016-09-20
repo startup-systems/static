4bc606ca9a0c940fa88c5a5d777ce3b3f03fa43d
Carolina Peisch <carolinapeisch@gmail.com>
2016-09-20 16:04:04 +0000

    Submitting static assignment v.1

--- generate.sh
+++ generate.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#./generate.sh inputdir outputdir
+
 set -ex
 
-# YOUR CODE HERE
+for file in examples/simple/*; do
+	head=$(head -1 $file) #isolating title
+	tail=$(tail -1 $file) #isolating body
+	sed 's/{{title}}/head/' template.html > $file.html # in template.html, replacing {{file}} with head, also creating a new html file
+	for file in examples/simple/*; do
+		find . -name "*.html" #find the newly created html files
+			cp "$file" outputdir #trying to copy those files to the outputdirectory - but not sure how to do
+			for file in outputdir; do #for the files in this outputdirectory
+			sed 's/{{body}}/tail/' "$file.html" #now it's the tail's turn to replace the {{body}}
+		done
+	done
+done
+
+
