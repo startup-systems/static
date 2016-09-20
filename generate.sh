c4adfbeaff2342be7ecb81ee1d9c856541322dac
arp122 <arpit.sabherwal@gmail.com>
2016-09-20 22:45:18 +0000

    All5

--- generate.sh
+++ generate.sh
@@ -9,12 +9,12 @@ do
                 if [ ! -d "$outputDir" ]; then
                         mkdir -p "$outputDir"
                 fi
-                outputFile=$(basename "$file" ".txt")
+                outputFile=$(basename "$file .txt")
                 echo "$template+$outputFile"
                 echo "$template" > "$outputDir/$outputFile.html"
                 sed -i "s/{{title}}/$( head -n 1 "$file")/g" "$outputDir/$outputFile.html" 
 sed -i  "s/{{body}}/$(tail -n +3 "$file")/g" "$outputDir/$outputFile.html"     
 
 #sed -i -e "s/{{body}}/$(tail -n +2 $file)/g" $outputDir"/"$outputFile".html"
-        done < "$file"
+        done< "$file"
 done
