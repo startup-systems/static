55807bc96cb2436c76f39012f6e0a4347a8f4249
jersyliu <jersyliu@outlook.com>
2016-09-20 18:58:03 +0000

    Jersyliu

--- generate.sh
+++ generate.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 set -ex
-base=`pwd`
 outdir=$2
-if [ ! -d $outdir ]
+if [ ! -d "$outdir" ]
 then
-	mkdir -p $outdir
+	mkdir -p "$outdir"
 fi
 
-files=(`ls $1`)
-num=${#files[@]}
+files=($("ls" "$1"))
 
 for i in "${files[@]}"
 do
-	NAME=`echo "$i" | cut -d'.' -f1`
-	echo $NAME
+	NAME=$(basename "$i")
 	cat template.html > "$2/$NAME.html"
-	subtopic=`head -n 1 $1/$i`
-	subbody=`tail -n 1 $1/$i`
-	sed -i "s!{{title}}!$subtopic!g" $2/$NAME.html
-	sed -i "s!{{body}}!$subbody!g" $2/$NAME.html
+	subtopic=$(head -n 1 "$1"/"$i")
+	subbody=$(tail -n 1 "$1"/"$i")
+	sed -i "s!{{title}}!$subtopic!g" "$2"/"$NAME".html
+	sed -i "s!{{body}}!$subbody!g" "$2"/"$NAME".html
 done
 
 
