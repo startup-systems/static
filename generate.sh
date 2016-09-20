05b7b9cf9a92b19d73004e0c1f005230328a38e5
Jaldeep Acharya <jka67@cornell.edu>
2016-09-20 16:49:25 +0000

    finished script

--- generate.sh
+++ generate.sh
@@ -2,4 +2,16 @@
 
 set -ex
 
-# YOUR CODE HERE
+
+READ_FOLDER=$1
+WRITE_FOLDER=$2
+rm -rf $WRITE_FOLDER
+mkdir -p $WRITE_FOLDER
+
+for file in $READ_FOLDER/*.txt
+do
+	t=`head -n 1 $file`							#Extract title
+	b=`tail -n +3 $file`						#Extract body
+	name=`basename $file .txt`					#Extract file name
+	sed "s/{{title}}/$t/g;s/{{body}}/$b/g" template.html > $WRITE_FOLDER/$name.html
+done
