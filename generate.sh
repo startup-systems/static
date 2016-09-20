b404f02da138625b61cbca6fe300910c5d944ada
Constantin Scholl <constantin@scholl.im>
2016-09-20 00:59:04 -0400

    update generate.sh

--- generate.sh
+++ generate.sh
@@ -2,4 +2,20 @@
 
 set -ex
 
-# YOUR CODE HERE
+function html() {
+    TITLE="$(head -n 1 $1)"
+    BODY="$(tail -n +2 $1)"
+    FILENAME=$(sed 's/txt/html/g' <<< $1)
+    # Paragraph formatting (?)
+    BODY=$(sed ':a;N;$!ba;s/\n/<p><\/p>/g' <<< $BODY)
+    # URL formatting
+    BODY=$(sed 's/\(https\?[^\s\\]*.com\(\/[^\s\\]*\)\?\)/<a href="\1">\1<\/a>/g' <<< $BODY)
+    sed -e "s@{{title}}@$TITLE@g; s@{{body}}@$BODY@g" $(pwd)/template.html > $FILENAME
+}
+export -f html
+
+rm -rf output
+cp -R ./$1 ./$2
+
+find ./$2 -type f -exec bash -c 'html "$0"' {} \;
+find ./$2 -name "*.txt" | xargs rm
