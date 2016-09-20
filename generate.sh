43fe866c2ecb9a96bec22e283ceb173b8f29772e
jersyliu <jersyliu@outlook.com>
2016-09-20 17:02:57 +0000

    Jersyliu

--- generate.sh
+++ generate.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
 set -ex
+base=`pwd`
+outdir=$2
+if [ ! -d $outdir ]
+then
+	mkdir -p $outdir
+fi
 
-# YOUR CODE HERE
+files=(`ls $1`)
+num=${#files[@]}
+
+for i in "${files[@]}"
+do
+	NAME=`echo "$i" | cut -d'.' -f1`
+	echo $NAME
+	cat template.html > "$2/$NAME.html"
+	subtopic=`head -n 1 $1/$i`
+	subbody=`tail -n 1 $1/$i`
+	sed -i "s!{{title}}!$subtopic!g" $2/$NAME.html
+	sed -i "s!{{body}}!$subbody!g" $2/$NAME.html
+done
+
+
