7429c12a165540528a299b3f75a3d362b5641a39
AltmerX <sabrina5426@live.com>
2016-09-20 05:21:47 +0000

    second commit

--- generate.sh
+++ generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -ex
-for filename in $1*.txt; do
-    title=$(head -n 1 $filename)
-    body=$(sed -n '3,$p' $filename)
-    htmlfile=$(basename $filename .txt)".html"
+for filename in $1/*.txt; do
+    title="$(head -n 1 $filename)"
+    body="$(sed -n '3,$p' $filename)"
+    htmlfile="$(basename $filename .txt).html"
     mkdir -p $2
     echo $filename | sed -e 's/{{body}}/'"$body"'/g' -e 's/{{title}}/'"$title"'/g' "template.html" > $2/$htmlfile
 done
