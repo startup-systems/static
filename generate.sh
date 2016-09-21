84d7cb758db38517f6ff1c77379937c66b2137d8
roycboyc <rc726@cornell.edu>
2016-09-21 03:43:47 +0000

    eleven forty three pm

--- generate.sh
+++ generate.sh
@@ -1,19 +1,17 @@
+#!/bin/bash
 
-in_dir=$1
-out_dir=$2
+set -ex
 
-if [ ! d "out_dir" ]
-then
- mkdir -p "$out_dir"
-fi
+indir=$1
+outdir=$2
 
-for filename in "$in_dir" /*
-do
-   title=$(head -1 $filename)
-   filebody=$(tail -n 1 $filename)
-   get_txtname=$(basename "$filename" .txt)
-   sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$filebody"'#g' template.html > "$out_dir/$get_txtname".html
-   
-done
+mkdir -p "$outdir"
 
-      
+for filename in $indir/*
+   do
+      title="$(head -n 1 "$filename")"
+      filebody="$(tail -n +3 "$filename")"
+      txtname="$(basename "$filename" .txt)"
+      sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$filebody"'#g' template.html > "$outdir/$txtname".html
+
+   done
