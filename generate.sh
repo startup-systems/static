93fc9d7bf2816365f9e13a40bf2ff0a9d46866f8
Shanshan Zhang <sz438@cornell.com>
2016-09-21 04:24:24 +0000

    add generate file

--- generate.sh
+++ generate.sh
@@ -2,4 +2,14 @@
 
 set -ex
 
-# YOUR CODE HERE
+# YOUR CODE H
+mkdir -p $2
+for file in $1/* ; do
+	echo item: $file
+	filename=$(basename "$file" .txt) 
+	title=$(head -1 $file) 
+	body=$(tail -n +3 $file)
+	cp template.html $2/$filename.html
+	cat $2/$filename.html | sed -i "s/{{title}}/$title/g" $2/$filename.html
+	cat $2/$filename.html | sed -i "s/{{body}}/$body/g" $2/$filename.html
+done
