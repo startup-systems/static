4cda8b8699329b2a13388957dedccd330be1fb93
interactivetech <interactivetech1@gmail.com>
2016-09-21 03:02:28 +0000

    Solution by Andrew Mendez aem

--- generate.sh
+++ generate.sh
@@ -3,3 +3,41 @@
 set -ex
 
 # YOUR CODE HERE
+
+#Print two arguments
+#1st is the input directory
+#2nd is the output directory
+a=1
+input=$1
+output=$2
+
+#echo "Input Dir:" $input
+#echo "Output Dir:" $output
+
+#Create Directory if does not exist
+mkdir -p  $2
+
+echo "Input Directory" $inputFile
+
+#Store output of title(first line) and body(last line)
+# in input text to variable
+
+for f in $1/*.txt
+do
+
+ echo $(basename $f)
+
+
+ inputFile=$(basename $f .txt)
+ title=$(sed -n '1p' $f)
+ body=$(sed -n '3,$p' $f)
+
+ echo $title
+ echo "Body:" $body
+
+ #Create html file with title and body 
+ outputFile=$output/$inputFile.html
+
+ sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" template.html > "$outputFile"
+ echo $outputFile $(file $outputFile)
+done
