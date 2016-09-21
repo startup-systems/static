87c054785b9c1ce0056f7fcf427de73a4ab1ee4b
42lince <42.duxiaoxi@gmail.com>
2016-09-21 18:29:43 +0000

    double quote

--- generate.sh
+++ generate.sh
@@ -2,24 +2,24 @@
 
 set -ex
 
-inputdir=$1/*
-outputdir=$2
+inputdir="$1/*"
+outputdir="$2"
 if [ ! -d "$outputdir" ]; then
-    mkdir -p $outputdir
+    mkdir -p "$outputdir"
 fi
 
 template=template.html
-for i in $inputdir; do
-    lines=$(sed -n $= $i)
-    title=$(head -1 $i)
+for i in "$inputdir"; do
+    lines="$(sed -n $= $i)"
+    title="$(head -1 $i)"
 
-    body=$(tail -n $(($lines-2)) $i)
+    body="$(tail -n $(($lines-2)) $i)"
     
-    oldfilename=$(basename $i)
-    newfilename=$(echo $oldfilename | sed s/txt/html/)
+    oldfilename="$(basename $i)"
+    newfilename="$(echo $oldfilename | sed s/txt/html/)"
 
-    sed "s/{{title}}/$title/g" $template > $outputdir/$newfilename
-    sed -i "s/{{body}}/$body/g" $outputdir/$newfilename
+    sed "s/{{title}}/$title/g" "$template" > "$outputdir/$newfilename"
+    sed -i "s/{{body}}/$body/g" "$outputdir/$newfilename"
     
     #extra credit
 	
