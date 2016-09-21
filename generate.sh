063c8b9cf2b543db477c9d93aedadc51bce2ed22
Tiwaree, Mani <Mani.Tiwaree@mastercard.com>
2016-09-20 21:03:37 -0400

    First commit with completed bash

--- generate.sh
+++ generate.sh
@@ -2,4 +2,12 @@
 
 set -ex
 
-# YOUR CODE HERE
+if [ -d "$1" ]; then
+  mkdir -p $2    
+  for i in "$1"/*.txt; do
+    title="$(head -n 1 $i)"
+    body="$(cat $i | tail -n 1)"
+    outputName=$(echo ${i##*/} | sed 's/txt/html/g')
+    sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" < template.html > $2/$outputName
+    done
+fi
