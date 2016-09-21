bb6157e54ac4654eddc0660970fbbf204c5b41a5
trishalaneeraj <trishalaneeraj@gmail.com>
2016-09-21 14:16:31 +0000

    added generate file

--- generate.sh
+++ generate.sh
@@ -2,4 +2,32 @@
 
 set -ex
 
-# YOUR CODE HERE
+# Declaring arguments. The script takes two arguments:
+# The input directory
+# The output directory
+inputDir="$1"
+outputDir="$2"
+
+
+template="$(<template.html)"
+#Creating output directory if it does not exist.
+if [ ! -d "$outputDir" ]; then
+  mkdir -p "$outputDir"
+fi
+
+
+
+#For each file in input directory..
+for file in "$inputDir"/*
+do
+
+  #Extracting file name from input file
+  filename=$(basename "$file" ".txt")
+
+  #Saving template to output directory
+  echo "$template" > "$outputDir/""$filename.html"
+
+	#Replacing title and body
+  sed -e "s/{{title}}/$( head -n 1 "$file")/g" "$outputDir/""$filename.html"
+  sed -e "s/{{body}}/$( tail -n +3 "$file")/g" "$outputDir/""$filename.html" #line 2 is empty
+done
