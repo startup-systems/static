fa76146ac73ea47e691972bc631bb3c7efdbebc3
interactivetech <interactivetech1@gmail.com>
2016-09-21 03:07:38 +0000

    cleaned usless echos, double quoted to prevent globbing and word splitting, and removed unused input

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ set -ex
 #Print two arguments
 #1st is the input directory
 #2nd is the output directory
-a=1
+
 input=$1
 output=$2
 
@@ -15,29 +15,29 @@ output=$2
 #echo "Output Dir:" $output
 
 #Create Directory if does not exist
-mkdir -p  $2
+mkdir -p  "$2"
 
-echo "Input Directory" $inputFile
+echo "Input Directory" "$inputFile"
 
 #Store output of title(first line) and body(last line)
 # in input text to variable
 
-for f in $1/*.txt
+for f in input/*.txt
 do
 
- echo $(basename $f)
+ #echo $(basename "$f")
 
 
- inputFile=$(basename $f .txt)
- title=$(sed -n '1p' $f)
- body=$(sed -n '3,$p' $f)
+ inputFile=$(basename "$f" .txt)
+ title=$(sed -n '1p' "$f")
+ body=$(sed -n '3,$p' "$f")
 
- echo $title
- echo "Body:" $body
+ #echo "$title"
+ #echo "Body:" "$body"
 
  #Create html file with title and body 
- outputFile=$output/$inputFile.html
+ outputFile="$output"/"$inputFile".html
 
  sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" template.html > "$outputFile"
- echo $outputFile $(file $outputFile)
+ #echo "$outputFile" $(file "$outputFile")
 done
