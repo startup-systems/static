9e218de347f612e909e0fbd3284b8f45278dd71b
Chen Zhang <recursion915@users.noreply.github.com>
2016-09-20 20:56:53 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,19 +4,19 @@ set -ex
 
 # YOUR CODE HERE
 # get the first and second arguments
-input="$1"
-output=$2
+input_dir="$1"
+output_dir=$2
 # make directory if it does not exist
-if [ ! -d "$output" ]; then
-  mkdir -p "$output"
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
 fi
 # apply basename to create new filename
-for file in "$input"/*
+for file in "$input_dir"/*
 do
   oldname=$(basename "$file" .txt)
   newname=$oldname.html
   title=$(head -n 1 "$file")
   body=$(tail -n 1 "$file")
 # replace title and body, then output the file
-  sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$body"'#g' template.html > "$output/$newname"
+  sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$body"'#g' template.html > "$output_dir/$newname"
 done
