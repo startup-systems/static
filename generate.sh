05fff73a29db6ff05c31b2278632967e7b31ada7
charlessuo <charlessuo@gmail.com>
2016-09-21 01:57:46 +0000

    first

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ set -ex
 anyfile=$1
 opt1=$2
 
-if [! -d "$opt1"]; then
+if [ ! -d "$opt1" ]; then
   mkdir -p "$opt1"
 fi
 
@@ -17,5 +17,5 @@ do
   cat template.html > "$2/$newf"
   title=$(head -n 1 $file)
   body=$(tail -n 1 $file)
-  sed -e 's/{{title}}/${title}/g' -e "s#{{body}}#${body}#g" $2/$newf 
+  sed -e 's/{{title}}/'"$title"'/g' -e "s#{{body}}#${body}#g" template.html > $2/$newf
 done
