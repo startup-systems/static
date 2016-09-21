1bd041354386f49f8cb2eb6be32b3f29be1aa54f
kristenwang <kristenwang23@gmail.com>
2016-09-20 22:14:03 -0400

    homework done

--- generate.sh
+++ generate.sh
@@ -3,3 +3,17 @@
 set -ex
 
 # YOUR CODE HERE
+input=$1
+output="$2"
+if [! -d $output ]; then
+   mkdir -p $output
+fi
+
+for file in $input*
+do
+  name=$(basename "$file"| cut -d. -f1)
+  title=$(head -n1 "$file")
+  body=$(tail -n+3 "$file")
+  outputname="$output/$name.html"
+  sed -e 's/{{title}}/'"$title"'/g' -e 's#{{body}}#'"$body"'#g' template.html > $outputname
+done
