f475c49c354598b596575b6f07944a256d0d7737
Nishad Prinja <np327@cornell.edu>
2016-09-20 20:03:01 -0400

    v1

--- generate.sh
+++ generate.sh
@@ -3,3 +3,20 @@
 set -ex
 
 # YOUR CODE HERE
+
+if [ ! -d $2 ]
+	then
+	mkdir $2
+fi
+
+for post in $1/*
+do
+
+    html="$(basename "$post" .txt).html"
+
+    titleField="$(head -n 1 "$post")"
+    bodyText="$(tail -n 1 "$post")"
+    
+    echo "$post" | sed -e 's/{{body}}/'"$bodyText"'/' -e 's/{{title}}/'"$titleField"'/' template.html >> "$2/$html"
+
+done
