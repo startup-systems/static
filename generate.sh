48ae14f5b493deae0d54d94a8d8d000358d9ab34
vagrant <utsavvakil@gmail.com>
2016-09-21 01:38:34 +0000

    Modified generate.sh file

--- generate.sh
+++ generate.sh
@@ -8,13 +8,11 @@ template=$(<template.html)
 
 for file in $1/*
 do
-        while read -r; do
                 if [ ! -d "$outputDirectory" ]; then
                         mkdir -p "$outputDirectory"
                 fi
-                outputFile="$(basename "$file" ".txt")"
+                outputFile=$(basename "$file" ".txt")
                 echo "$template" > "$outputDirectory/""$outputFile.html"
                 sed -i -e "s/{{title}}/$( head -n 1 "$file")/g" "$outputDirectory/""$outputFile.html"
                 sed -i -e "s/{{body}}/$( tail -n +3 "$file")/g" "$outputDirectory/""$outputFile.html"
-        done<"$file"
 done
