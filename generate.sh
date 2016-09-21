4ac3564ef200d95d69b6cd807ed97b9d240882f1
vagrant <jz762@cornell.edu>
2016-09-21 05:20:30 +0000

    static site generator-v1

--- generate.sh
+++ generate.sh
@@ -3,3 +3,28 @@
 set -ex
 
 # YOUR CODE HERE
+
+#for file in $1/*.txt;
+indir=$1
+outdir=$2
+#check if directory exists (chttp://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shel#l-script)
+if [ ! -d $outdir ]; then
+ mkdir $outdir
+fi
+
+for file in $(ls $indir)
+do
+  filename=$(basename ${file})
+  filename=${filename%.*}
+
+  title=$(head -1 $indir/$file)  
+  body=$(tail -1 $indir/$file)
+
+# cat template.html > "$outdir/${filename}.html"#why did this not work????
+
+  sed -e 's/{{title}}/'"$title"'/' -e 's/{{body}}/'"$body"'/'  template.html >> "${outdir}/${filename}.html"  
+
+done
+
+
+
