4ebf04a8bd92bb8c4078bd8d1d50db7199702703
Bruce-Yu-Xi <xy333@cornell.edu>
2016-09-20 23:25:42 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -13,6 +13,6 @@ for file in "$file_input"/*.txt
 do
         html_n=$(basename "$file" .txt)
         Head=$(head -n 1 "$file")
-        Tail=$(tail -n 3 "$file")
-        sed -e 's#{{title}}#"$Head"#' -e 's#{{body}}#"$Tail"#' template.html > "$file_output"/"$html_n".html
+        Tail=$(tail -n+3 "$file")
+        sed -e "s#{{title}}#""$Head""#;s#{{body}}#""$Tail""#" template.html > "$file_output"/"$html_n".html
 done
