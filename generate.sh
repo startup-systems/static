b5345455e7776612ee6a9a19f988b568fee5ff93
Carolina Peisch <carolinapeisch@gmail.com>
2016-09-21 18:39:35 +0000

    Another attempt

--- generate.sh
+++ generate.sh
@@ -4,15 +4,18 @@
 
 set -ex
 inputdir=$1
-outputdirectory=$2
+outputdir=$2
+made_directory=$3 #if not directory exists, need to make one
 
-for file in `ls $inputdir/*.txt`; do
-	head=$(head -1 $file) #isolating title
-	tail=$(tail -n +3 $file) #isolating body
-	sed 's/{{title}}/$head/' template.html > $outputdir filename=$(basename "$fullfilename")
-	for filename in $outputdir; do
-		sed 's/{{body}}/$tail/' $filename > $finalfile
+if [! -d "$outputdir"]; then #first check if directory exists, if not, need to make one
+	mkdir -p "$made_directory"
+fi 
+for file in "$inputdir"/*.txt; do #for a file in inputdir, 
+	filename="$(basename "$file" ".txt")" #rename with basename and make sure is a text file
+	head=$(head -1 "$file") #isolating title
+	tail=$(tail -n +3 "$file") #isolating body
+	sed 's/{{title}}/$head/' template.html > filename
+	for filename in "$inputdir"/*.txt; do
+		sed 's/{{body}}/$tail/' "$newfile" > "$outputdir/$filename.html" 
 	done
-done
-
-
+done 
