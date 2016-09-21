4e8aed85f978c9f6141c8faa6e268e111fcee552
Mingyu Zhang <amberzhangx@gmail.com>
2016-09-21 17:10:48 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-
+# ./generate.sh examples/simple/ output/
 set -ex
 
 # YOUR CODE HERE
+input_dir=$1
+output_dir=$2
+mkdir -p $output_dir # if not exist
+
+
+for f in `ls $1`
+do
+	title=`head -n1 $input_dir/$f`
+	body=`tail -n1 $input_dir/$f`
+
+	html=`sed "s/{{title}}/$title/" template.html`
+	html=`echo $html | sed "s/{{body}}/$body/"`
+
+
+	f="${f%.*}" # wihout extension
+	newF=$output_dir$f.html
+	touch $newF
+	echo $html >> $newF
+
+done
