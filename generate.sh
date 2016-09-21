a2b02498310a46fcb72b223f9cc785a936123797
arp122 <arpit.sabherwal@gmail.com>
2016-09-21 01:23:28 +0000

    All6

--- generate.sh
+++ generate.sh
@@ -13,8 +13,8 @@ do
                 echo "$template+$outputFile"
                 echo "$template" > "$outputDir/$outputFile.html"
                  sed -i -e "s/{{title}}/$( head -n 1 "$file")/g" "$outputDir/$outputFile.html" 
-  sed -i -e  "s/{{body}}/$(tail -n +3 "$file")/g" "$outputDir/$outputFile.html"
+  sed -i -e "s/{{body}}/$(tail -n +3 "$file")/g" "$outputDir/$outputFile.html"
 
 #sed -i -e "s/{{body}}/$(tail -n +2 $file)/g" $outputDir"/"$outputFile".html"
-        done<"$file"
+        done<$file
 done
