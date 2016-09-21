1ef5d947fa75845130dcb1616394bbc300b649b0
Osasu Eboh <osawill@gmail.com>
2016-09-21 03:16:09 +0000

    Initial solution

--- generate.sh
+++ generate.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
+set -e
+#Make directories recursively
+mkdir -p "$2"
 
-set -ex
+for f in $1/*
+do
+    count=0
+    echo "Converting $f to $(basename -s .txt "$f").html"
+    #Read line by line
+    while IFS='' read -r temp || [[ -n "$temp" ]]; do
+        #echo "Text read from file: $temp"
+        if [ $count == 0 ]
+        then
+            title=$temp
+        elif [ $count -ge 2 ]
+        then
+            body="$temp" #concatenate each subsequent line
+        fi
+        
+        count=$((count+1))
+    done < "$f"
+    #Export using template
+    echo "<!DOCTYPE html> \
+            <html> \
+            <head> \
+                <meta charset=\"utf-8\"> \
+                <title>$title</title> \
+            </head> \
+            <body>$body</body> \
+            </html>" > "$2/$(basename -s .txt "$f").html"
+done
 
-# YOUR CODE HERE
+#Sources:
+#http://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
