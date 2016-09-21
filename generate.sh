0d88ee11cec57c56145badad1818549dd41b54d3
xidorn7 <souzimae@gmail.com>
2016-09-21 20:38:15 +0000

    ÃUpdate3

--- generate.sh
+++ generate.sh
@@ -4,15 +4,18 @@ set -ex
 
 inPath="$1"
 outPath="$2"
+
 mkdir -p "$2"
 
 # YOUR CODE HERE
 for file in $inPath/*.txt; do
 #
-        filename="$(basename $file)"
-        outfilename="$(echo $filename | cut -f 1 -d '.')"
+        filename=$(basename "$file")
+        outfilename=$(echo "$filename" | cut -f 1 -d '.')
         out="$outPath/$outfilename".html
-        sed -e "s/{{title}}/$(head -n 1 $inPath/$filename)/g" -e "s/{{body}}/$(tail -n +3 $inPath/$filename)/g" template.html > "$out"
+        title="$(head -n 1 $inPath/$filename)"
+	body="$(tail -n +3 $inPath/$filename)"
+	sed -e s/{{title}}/"$title"/g -e s/{{body}}/"$body"/g template.html > "$out"
 done    
 
 
