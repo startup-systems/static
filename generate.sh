03f95927fc4adfed7383b8d46917fa59c5b882dc
Tiwaree, Mani <Mani.Tiwaree@mastercard.com>
2016-09-20 21:46:12 -0400

    Fixed the last two bugs

--- generate.sh
+++ generate.sh
@@ -6,8 +6,8 @@ if [ -d "$1" ]; then
   mkdir -p "$2"    
   for i in "$1"/*.txt; do
     title=$(head -n 1 "$i")
-    body=$(cat "$i" | tail -n 1)
-    outputName=$(echo "${i##*/}" | sed "s/txt/html/g")
+    body=$(tail -n 1 "$i")
+    outputName=$(basename "$i" | sed "s/txt/html/g")
     sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" < "template.html" > "$2/$outputName"
     done
 fi
