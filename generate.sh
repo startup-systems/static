fd2ad1111e41e05922707ce7fb87a873b434493e
xidorn7 <souzimae@gmail.com>
2016-09-21 20:50:04 +0000

    ÃUpdate4

--- generate.sh
+++ generate.sh
@@ -13,8 +13,8 @@ for file in $inPath/*.txt; do
         filename=$(basename "$file")
         outfilename=$(echo "$filename" | cut -f 1 -d '.')
         out="$outPath/$outfilename".html
-        title="$(head -n 1 $inPath/$filename)"
-	body="$(tail -n +3 $inPath/$filename)"
+        title=$(head -n 1 "$inPath/$filename")
+	body=$(tail -n +3 "$inPath/$filename")
 	sed -e s/{{title}}/"$title"/g -e s/{{body}}/"$body"/g template.html > "$out"
 done    
 
