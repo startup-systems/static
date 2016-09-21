342e3fe24e01eaa725631cb2c01171fb6d9a75b6
ragodrich <rag299@cornell.edu>
2016-09-21 20:31:53 +0000

    ragSubmission

--- generate.sh
+++ generate.sh
@@ -3,3 +3,19 @@
 set -ex
 
 # YOUR CODE HERE
+inputfile=$1
+outputfile=$2
+
+mkdir -p $2
+ 
+for file in "$1"/*.txt
+do
+	title=$( head -n 1 "$file")
+	body=$( tail -n +3 "$file")
+	f=$(basename "$file" .txt).html
+
+	cp template.html "$2"/"$f"
+	sed -i "s/{{title}}/$title/g" "$2"/"$f"
+	sed -i "s/{{body}}/$body/g" "$2"/"$f" 
+
+done
