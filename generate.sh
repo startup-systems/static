367ee3cbbd544c454208f1423c425a1aa31cb3e9
roycboyc <rc726@cornell.edu>
2016-09-21 01:09:34 +0000

    first

--- generate.sh
+++ generate.sh
@@ -3,4 +3,28 @@
 set -ex
 
 # YOUR CODE HERE
-echo "Hello Wordl!"
+
+#!/bin/bash
+
+set -ex
+
+# YOUR CODE HERE
+in_dir  =  $1
+out_dir  =  $2
+
+if [! -d "$out_dir"] ; then
+   mkdir -p "$out_dir"
+fi
+
+for filename in "$in_dir" /*
+do
+   title  =  $(head -1 "$in_dir/$filename")
+   filebody  =  $(tail -n +1 "$in_dir/$filename")
+   get_txtname  =  $(basename $in_dir/filename.txt .txt)
+   htmlname  =  $get_txtname.html
+   sed -e 's#{{title}}#' "$title" '/g' -e 's#{{body}}#' "$filebody" '#g' template.html > "$out_dir"/"$filename".html
+   
+done
+
+exit
+   
