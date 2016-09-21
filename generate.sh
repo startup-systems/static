cd0127029c7c501db8e31d023d2cd601f2bdfbf9
roycboyc <rc726@cornell.edu>
2016-09-21 01:57:35 +0000

    iv attempt

--- generate.sh
+++ generate.sh
@@ -1,29 +1,24 @@
-#!bin/bash
-
-set -ex
-
-# YOUR CODE HERE
 #!/bin/bash
 
 set -ex
 
 # YOUR CODE HERE
-out_dir=$2
-in_dir=$1
 
-if [ ! -d "$out_dir" ] ; then
-   mkdir -p "$out_dir"
+in_dir=$1
+out_dit=$2
+
+if [ ! -d "$out_dir" ]; then
+     mkdir -p "$out_dir"
 fi
 
-for filename in "$in_dir" /*
+for filename in "$in_dir" /*.txt
+
 do
-   title=$(head -1 "$in_dir/$filename")
-   filebody=$(tail -n +3 "$in_dir/$filename")
-   get_txtname=$(basename "$filename" .txt)
-   htmlname=$get_txtname.html
-   sed -e 's#{{title}}#' "$title" '/g' -e 's#{{body}}#' "$filebody" '#g' template.html > "$out_dir"/"$htmlname".html
+   title="$(head -n 1 "$filename")"
+   filebody="$(tail -n +1 "$filename")"
+   get_txtname="$(basename "$filename" .txt)"
+   sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$filebody"'#g' template.html > "$out_dir"/"$get_txtname".html
    
 done
 
-exit
       
