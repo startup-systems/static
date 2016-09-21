0a20e06336aa559e79a0fe54f46665f1eac046a9
Tiwaree, Mani <Mani.Tiwaree@mastercard.com>
2016-09-20 21:30:56 -0400

    Changed the script to pass codeclimate test

--- generate.sh
+++ generate.sh
@@ -3,11 +3,11 @@
 set -ex
 
 if [ -d "$1" ]; then
-  mkdir -p $2    
+  mkdir -p "$2"    
   for i in "$1"/*.txt; do
-    title="$(head -n 1 $i)"
-    body="$(cat $i | tail -n 1)"
-    outputName=$(echo ${i##*/} | sed 's/txt/html/g')
-    sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" < template.html > $2/$outputName
+    title=$(head -n 1 "$i")
+    body=$(cat "$i" | tail -n 1)
+    outputName=$(echo "${i##*/}" | sed "s/txt/html/g")
+    sed -e "s/{{title}}/$title/" -e "s/{{body}}/$body/" < "template.html" > "$2/$outputName"
     done
 fi
