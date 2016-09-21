ae1b5f34c0116fa4cedef57a5175c17255450d13
roycboyc <rc726@cornell.edu>
2016-09-21 01:29:44 +0000

    ii

--- generate.sh
+++ generate.sh
@@ -9,22 +9,27 @@ set -ex
 set -ex
 
 # YOUR CODE HERE
-in_dir  =  $1
-out_dir  =  $2
+#!/bin/bash
 
-if [! -d "$out_dir"] ; then
+set -ex
+
+# YOUR CODE HERE
+out_dir=$2
+in_dir=$1
+
+if [! -d "$out_dir" ] ; then
    mkdir -p "$out_dir"
 fi
 
 for filename in "$in_dir" /*
 do
-   title  =  $(head -1 "$in_dir/$filename")
-   filebody  =  $(tail -n +1 "$in_dir/$filename")
-   get_txtname  =  $(basename $in_dir/filename.txt .txt)
-   htmlname  =  $get_txtname.html
+   title=$(head -1 "$in_dir/$filename")
+   filebody=$(tail -n +3 "$in_dir/$filename")
+   get_txtname=$(basename "$filename" .txt)
+   htmlname=$get_txtname.html
    sed -e 's#{{title}}#' "$title" '/g' -e 's#{{body}}#' "$filebody" '#g' template.html > "$out_dir"/"$filename".html
    
 done
 
 exit
-   
+      
