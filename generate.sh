0f56e27faa2465076f0865d3238c0cd7a6419a6b
Osasu Eboh <osawill@gmail.com>
2016-09-21 19:35:49 +0000

    Fixed meta tag

--- generate.sh
+++ generate.sh
@@ -25,7 +25,7 @@ do
         <!DOCTYPE html>
         <html>
           <head>
-            <meta charset=/"utf-8/">
+            <meta charset="utf-8">
             <title>$title</title>
           </head>
           <body>
