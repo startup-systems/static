472f67482b4b69ad49989505067369303c2500e2
dydcfg <dydcfg@gmail.com>
2016-09-21 03:09:02 +0000

    Test passed

--- generate.sh
+++ generate.sh
@@ -2,4 +2,18 @@
 
 set -ex
 
-# YOUR CODE HERE
+inputPath=$1
+outputPath=$2
+
+if [ ! -d "$outputPath" ];then
+  mkdir -p "$outputPath"
+fi
+
+for file in "$inputPath"/* ; do
+  fileName=$(basename "$file" .txt)
+  fileName="${fileName%.*}"
+  outputName="$fileName.html"
+  title=$(head -n 1 "$file")
+  body=$(tail -n+3 "$file")
+  sed -e 's/{{title}}/'"$title"'/' -e 's/{{body}}/'"$body"'/' template.html >> "$outputPath"/"$outputName"
+done
