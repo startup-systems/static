8b6112b453c0c2f8f5d9f6ec900ce2e74eab2709
vagrant <jz762@cornell.edu>
2016-09-21 05:30:59 +0000

    double quote is important!

--- generate.sh
+++ generate.sh
@@ -8,17 +8,17 @@ set -ex
 indir=$1
 outdir=$2
 #check if directory exists (chttp://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shel#l-script)
-if [ ! -d $outdir ]; then
- mkdir $outdir
+if [ ! -d "$outdir" ]; then
+ mkdir "$outdir"
 fi
 
-for file in $(ls $indir)
+for file in "$indir"/*
 do
-  filename=$(basename ${file})
+  filename=$(basename "${file}")
   filename=${filename%.*}
 
-  title=$(head -1 $indir/$file)  
-  body=$(tail -1 $indir/$file)
+  title=$(head -1 "$indir"/"$file")  
+  body=$(tail -1 "$indir"/"$file")
 
 # cat template.html > "$outdir/${filename}.html"#why did this not work????
 
