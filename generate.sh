22e1de70999e7ee480ea9a25a0abbf7d51bc65a4
Krystalllll <jrjyyzy@gmail.com>
2016-09-21 19:17:27 +0000

    try one

--- generate.sh
+++ generate.sh
@@ -1,5 +1,19 @@
-#!/bin/bash
+#!/bin/baish
 
 set -ex
 
 # YOUR CODE HERE
+input = "$1"
+if [ ! -d "$2" ] 
+then
+	mkdir -p $2
+fi
+for file in "$input"/*
+do
+	title=$(head -n 1 $file)
+        body=$(tail -n 1 $file) 
+filename=$(basename $file .txt).html
+sed 's/{{title}}/'"$title"'/g'template.html > "$2/$filename"
+sed 's/{{body}}/'"$body"'/g'template.html> "$2/$filename"
+done
+
