342e58500f7ac14781f4af142314e0ee2fbd1d12
gautamc1106 <gc463@cornell.edu>
2016-09-21 20:42:37 +0000

    Updated file

--- generate.sh
+++ generate.sh
@@ -4,15 +4,14 @@
 mkdir -p "$2"
 for file in "$1"/*;
 do
-  line=$(head -n 1 $file)
-  count=$(sed -n '$=' $file)
+  line=$(head -n 1 "$file")
+  count=$(sed -n '$=' "$file")
   counts=$((count - 1))
-  main=$(head -n $count $file | tail -n $counts)
-  main=`echo $main | tr '\n' "\\n"`
-  fname=$(basename $file .txt)
-  echo ""> $fname.html
-  cp template.html $2/$fname.html
-  sed -i "s/{{title}}/$line/g" $2/$fname.html
-  sed -i "s/{{body}}/$main/g" $2/$fname.html
-  cat $fname.html
+  main=$(head -n "$count" $file | tail -n "$counts")
+  main=$(echo $main | tr '\n' "\\n")
+  fname=$(basename "$file" .txt)
+  echo ""> "$fname.html"
+  cp template.html "$2/$fname.html"
+  sed -i "s/{{title}}/$line/g" "$2/$fname.html"
+  sed -i "s/{{body}}/$main/g" "$2/$fname.html"
 done
