348165d0d1c66b62642909672e26de720b53f56b
krajabi <krajabi09@gmail.com>
2016-09-22 19:01:46 +0000

    submission

--- generate.sh
+++ generate.sh
@@ -2,6 +2,23 @@
 
 set -ex
 
-# YOUR CODE HERE
+#set input
+input=$1
 
-echo "Hello World!"
+#set output
+output=$2
+
+#check for directory
+if [ ! -d "$directory" ]; then
+	mkdir -p $2
+fi
+
+#pathname for the file
+name=$(basename "$file".txt | cut -d -f1)
+
+#adjust name
+for filename in $1/*.txt
+	do
+		title=$(head -n 1 "$file")
+		body=$(tail -n + 3 "$file")
+		sed -e 's/{{title}}/'"$title"'/ -e 's/{{body}}/'"$body"''/template.html > "$output".html	
