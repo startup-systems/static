d5e1b568f249e4ae68a345b2e2b637ca2e916a93
Mingyu Zhang <amberzhangx@gmail.com>
2016-09-21 22:47:05 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,12 @@
-src=$1
-des=$2
-mkdir -p "$des"
+#!/bin/bash
+input_dir=$1
+output_dir=$2
+mkdir -p "$output_dir"
   
-for file in $src/*.txt
+for file in $input_dir/*.txt
 do
 	title="$(head -n 1 "$file")"
 	body="$(tail -n +3 "$file")"
-    filename="$(basename "$file" .txt)"
-	sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$body"'#g' template.html > "$des"/"$filename".html
+	filename="$(basename "$file" .txt)".html
+	sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$body"'#g' template.html > "$output_dir$filename"
 done
