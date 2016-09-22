71a4c0bb261b41438d34986c4ab06aae089bf2a8
Mingyu Zhang <amberzhangx@gmail.com>
2016-09-21 23:40:24 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -8,5 +8,5 @@ do
 	title="$(head -n 1 "$file")"
 	body="$(tail -n +3 "$file")"
 	filename="$(basename "$file" .txt)".html
-	sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$body"'#g' template.html > "$output_dir$filename"
+	sed 's#{{title}}#'"$title"'#g;s#{{body}}#'"$body"'#g' template.html > "$output_dir"/"$filename"
 done
