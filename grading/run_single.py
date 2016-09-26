3c86ef12a0488ee16626a239b7ee152e0b498384
Aidan Feldman <alf239@cornell.edu>
2016-09-26 06:23:40 +0000

    handle bad pull request URL inputs

--- grading/run_single.py
+++ grading/run_single.py
@@ -11,7 +11,9 @@ PULL_REQUEST_URL = sys.argv[1]
 def create_pull_request(html_url):
     regex = re.compile('https://github\.com/([^/]+/[^/]+)/pull/(\d+)')
     match = regex.match(html_url.strip())
-    # TODO handle bad URL
+    if match is None:
+        raise ValueError("Invalid pull request URL.")
+
     repo = match.group(1)
     pr_number = match.group(2)
 
