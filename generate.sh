f4754b04aa2192799df0978cf70b7c7f9147546b
krajabi <krajabi09@gmail.com>
2016-09-28 23:09:31 +0000

    submit3

--- generate.sh
+++ generate.sh
@@ -2,4 +2,21 @@
 
 set -ex
 
-# YOUR CODE HERE
+#make directory
+mkdir -p $2
+
+#set input
+input="$1"
+
+#set output
+output="$2"
+
+#pathname for the file
+name=$(basename "$file".txt | cut -d -f1)
+
+#adjust name
+for filename in $1/*.txt
+	do
+		title="$(head -n 1 "$file")"
+		body="$(tail -n + 3 "$file")"
+		sed -e 's/{{title}}/'"$title"'/ -e 's/{{body}}/'"$body"''/template.html > $2/"$output"
