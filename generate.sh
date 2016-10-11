46266defc7c2ece9403c5928b98eb3e11a618615
Carolina Peisch <carolinapeisch@gmail.com>
2016-10-11 23:40:04 +0000

    codeclimate is fixed

--- generate.sh
+++ generate.sh
@@ -9,5 +9,5 @@ outputdir=$2
 for file in "$inputdir"/*.txt; do #for a file in inputdir, 
 	fileTitle="$(head -n 1 "$file")" #isolate title
 	fileBody="$(tail -n +3 "$file")" #isolate body
-	sed "s/{{title}}/$fileTitle/g" "template.html" | sed "s/{{body}}/$fileBody/g" > "$outputdir"/$("basename" "$file" ".txt").html
+	sed "s/{{title}}/$fileTitle/g" "template.html" | sed "s/{{body}}/$fileBody/g" > "$outputdir"/"$("basename" "$file" ".txt")".html
 	done 
