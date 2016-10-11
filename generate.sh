dc57a9ac065db77ebaf6754c23a6c09b64420be0
krajabi <krajabi09@gmail.com>
2016-10-11 23:16:30 +0000

    test6

--- generate.sh
+++ generate.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
 set -ex
-
-#makeDir
-if [ ! -d "$output" ];
-	then mkdir -p "$output"
-fi
 
 #set input
 input=$1
@@ -13,12 +7,25 @@ input=$1
 #set output
 output=$2
 
+#makeDir
+if [ ! -d "$output" ];
+	then mkdir -p "$output"
+fi
+
+
 #adjust name and create file
-for filename in "$input"/*;
+for filename in $input/*.txt;
 	do
-		filE=$(basename "$filename" .txt)
-		filE="${filE%.*}"
-		title=$(head -n 1 "$filename")
-		body=$(tail -n+3 "$filename")
-		sed -e 's/{{title}}/'"$title"'/' -e 's/{{body}}/'"$body"'/' template.html >> "$output"/"$filE"
+		filE="$(basename "${filename%.*}")".html;
+		#title
+		title=$(head -n 1 "$filename");
+		#body
+		body=$(tail -n+3 "$filename");
+		
+		#cat template
+		cat template.html > "$output/$filE";
+
+		#replace
+		sed -i -e "s,{{title}}, $title,g" "$output/$filE";
+		sed -i -e "s,{{body}}, $body,g" "$output/$filE";
 done
