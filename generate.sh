1ca027ddeea327835c70eac4718fbb4680e6e148
avash <avash.rao@gmail.com>
2016-11-24 20:40:04 -0500

    prevents globbing

--- generate.sh
+++ generate.sh
@@ -14,15 +14,15 @@ DEST_DIR=$2;
 # replace all urls into links
 # any blank lines followed by more content should create a new paragraph
 
-rm -rf $DEST_DIR;
-mkdir -p $DEST_DIR;
+rm -rf "$DEST_DIR";
+mkdir -p "$DEST_DIR";
 
 # for each file in SOURCE_DIR lets read title and body and use txt_to_html() to generate files
 for file in "$SOURCE_DIR"/*; do
-    filename=$(basename $file .txt);
+    filename=$(basename "$file" .txt);
     
-    title=$(sed -n '1p' $file);
-    body=$(sed -n '3, $p' $file);
+    title=$(sed -n '1p' "$file");
+    body=$(sed -n '3, $p' "$file");
 
     sed -e "s/{{title}}/$title/" < template.html > tmpfile;
     sed -e "s/{{body}}/$body/" < tmpfile > $DEST_DIR/$filename.html;
